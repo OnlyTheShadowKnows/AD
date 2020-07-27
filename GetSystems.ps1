0d761cdfc381ec297526588c81d4445ffccc50b5
OnlyTheShadowKnows <68879244+OnlyTheShadowKnows@users.noreply.github.com>
2020-07-27 19:32:06 -0400

    Update GetSystems.ps1

--- GetSystems.ps1
+++ GetSystems.ps1
@@ -1,3 +1,4 @@
+#(C) 2020 BlueTeamHandbook.com / Don Murdoch
 New-Item -Path C:\OTSK
 “ComputerName” | OutFile -FilePath C:\OTSK\List_of_Targets.CSV
 Get-ADComputer -filer ‘Name -like “*pattern*”’ | Select-Object -ExpandProperty Name | OutFile -FilePath C:\OTSK\List_of_Targets.csv -append
