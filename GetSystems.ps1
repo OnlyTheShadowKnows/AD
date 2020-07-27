0723188aa941dbb02d5b465d956d43a7cbf38371
OnlyTheShadowKnows <68879244+OnlyTheShadowKnows@users.noreply.github.com>
2020-07-27 19:50:10 -0400

    Update GetSystems.ps1

--- GetSystems.ps1
+++ GetSystems.ps1
@@ -1,4 +1,9 @@
 #(C) 2020 BlueTeamHandbook.com / Don Murdoch
+# RSAT is needed.
 New-Item -Path C:\OTSK
 “ComputerName” | OutFile -FilePath C:\OTSK\List_of_Targets.CSV
 Get-ADComputer -filer ‘Name -like “*pattern*”’ | Select-Object -ExpandProperty Name | OutFile -FilePath C:\OTSK\List_of_Targets.csv -append
+
+# or
+
+Get-ADComputer -Filter * | select DistinguishedName, DNSHostName, Name
