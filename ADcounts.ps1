455250206949d933fcb780ab73e769926720ae21
OnlyTheShadowKnows <68879244+OnlyTheShadowKnows@users.noreply.github.com>
2020-07-28 18:20:43 -0400

    Update ADcounts.ps1

--- ADcounts.ps1
+++ ADcounts.ps1
@@ -1,5 +1,5 @@
 # (C) 2020 BlueTeamHandbook.com Don Murdoch
 Import-Module ActiveDirectory
 (Get-ADUser -Filter *).Count 
-(Get-ADGroups -Filter *).Count
+(Get-ADGroup -Filter *).Count
 (Get-ADComputer -Filter *).Count
