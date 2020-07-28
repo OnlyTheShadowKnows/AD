24c7335cd39ff24807b1ba85b83f7b0322e6449f
OnlyTheShadowKnows <68879244+OnlyTheShadowKnows@users.noreply.github.com>
2020-07-27 17:55:33 -0700

    Update GetSystems.ps1

--- GetSystems.ps1
+++ GetSystems.ps1
@@ -1,9 +1,13 @@
 #(C) 2020 BlueTeamHandbook.com / Don Murdoch
 # RSAT is needed.
-New-Item -Path C:\OTSK
-“ComputerName” | OutFile -FilePath C:\OTSK\List_of_Targets.CSV
+Import-Module ActiveDirectory
+
+$out = $env:HOMEDRIVE + $env:HOMEPATH + "\OSTK"
+New-Item -ItemType Directory $out
+
+“ComputerName” | OutFile -FilePath $out\List_of_Targets.CSV
 Get-ADComputer -filer ‘Name -like “*pattern*”’ | Select-Object -ExpandProperty Name | OutFile -FilePath C:\OTSK\List_of_Targets.csv -append
 
 # or
 
-Get-ADComputer -Filter * | select DistinguishedName, DNSHostName, Name
+Get-ADComputer -Filter * | select DistinguishedName, DNSHostName, Name | export-csv -path $out\List_of_Targets.CSV
