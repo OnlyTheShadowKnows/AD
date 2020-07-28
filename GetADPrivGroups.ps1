e56138e5ea399e0758ba667dbf217beb9a04dff3
OnlyTheShadowKnows <68879244+OnlyTheShadowKnows@users.noreply.github.com>
2020-07-27 20:20:31 -0400

    Update GetADPrivGroups.ps1

--- GetADPrivGroups.ps1
+++ GetADPrivGroups.ps1
@@ -1,8 +1,12 @@
 # (c) 2020 BlueTeamHandbook.com Don Murdoch
 
-Get-ADGroupMember -identity "Domain Admins" | select objectClass,SamAccountName,name,distinguishedName | Export-csv -path C:\tmp\Groupmembers.csv -NoTypeInformatin
-Get-ADGroupMember -identity "Enterprise Admins" | select objectClass,SamAccountName,name,distinguishedName | Export-csv -path C:\tmp\Groupmembers.csv -NoTypeInformatin -append
-Get-ADGroupMember -identity "Schema Admins" | select objectClass,SamAccountName,name,distinguishedName | Export-csv -path C:\tmp\Groupmembers.csv -NoTypeInformatin -append
-Get-ADGroupMember -identity "Account Operators" | select objectClass,SamAccountName,name,distinguishedName | Export-csv -path C:\tmp\Groupmembers.csv -NoTypeInformatin -append
-Get-ADGroupMember -identity "Backup Operators" | select objectClass,SamAccountName,name,distinguishedName | Export-csv -path C:\tmp\Groupmembers.csv -NoTypeInformatin -append
-Get-ADGroupMember -identity "Group Policy Creator Owners" | select objectClass,SamAccountName,name,distinguishedName | Export-csv -path C:\tmp\Groupmembers.csv -NoTypeInformatin -append
+Import-Module ActiveDirectory
+
+New-Item c:\OTSK
+
+Get-ADGroupMember -identity "Domain Admins" | select objectClass,SamAccountName,name,distinguishedName | Export-csv -path c:\OTSK\Groupmembers.csv -NoTypeInformation
+Get-ADGroupMember -identity "Enterprise Admins" | select objectClass,SamAccountName,name,distinguishedName | Export-csv -path c:\OTSK\Groupmembers.csv -NoTypeInformation -append
+Get-ADGroupMember -identity "Schema Admins" | select objectClass,SamAccountName,name,distinguishedName | Export-csv -path c:\OTSK\Groupmembers.csv -NoTypeInformation -append
+Get-ADGroupMember -identity "Account Operators" | select objectClass,SamAccountName,name,distinguishedName | Export-csv -path c:\OTSK\Groupmembers.csv -NoTypeInformation -append
+Get-ADGroupMember -identity "Backup Operators" | select objectClass,SamAccountName,name,distinguishedName | Export-csv -path c:\OTSK\Groupmembers.csv -NoTypeInformation -append
+Get-ADGroupMember -identity "Group Policy Creator Owners" | select objectClass,SamAccountName,name,distinguishedName | Export-csv -path c:\OTSK\Groupmembers.csv -NoTypeInformation -append
