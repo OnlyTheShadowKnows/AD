4a141699818a030d92611ebf1a2f45c9059b4479
OnlyTheShadowKnows <68879244+OnlyTheShadowKnows@users.noreply.github.com>
2020-07-28 18:38:49 -0700

    Update GetSystems.ps1

--- GetSystems.ps1
+++ GetSystems.ps1
@@ -3,11 +3,12 @@
 Import-Module ActiveDirectory
 
 $out = $env:HOMEDRIVE + $env:HOMEPATH + "\OSTK"
-New-Item -ItemType Directory $out
+if (-not (Test-path -Path $out -PathType Container )) {
+   New-Item -ItemType Directory 
+}
+# make header
+select “Name” | Export-CSV -Path $out\List_of_Targets.CSV -NoTypeInformation
+Get-ADComputer -Filter 'Name -like "*AD*"' | Select Name | Export-Csv -Path $out\List_of_Targets.CSV -append
 
-“ComputerName” | OutFile -FilePath $out\List_of_Targets.CSV
-Get-ADComputer -filer ‘Name -like “*pattern*”’ | Select-Object -ExpandProperty Name | OutFile -FilePath C:\OTSK\List_of_Targets.csv -append
-
-# or
-
-Get-ADComputer -Filter * | select DistinguishedName, DNSHostName, Name | export-csv -path $out\List_of_Targets.CSV
+# or as an alternate
+Get-ADComputer -Filter * | select DistinguishedName, DNSHostName, Name | export-csv -path $out\List_of_Targets2.CSV
